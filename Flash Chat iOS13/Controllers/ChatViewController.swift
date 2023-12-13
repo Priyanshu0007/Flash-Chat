2dcf44d6cccb76611a7fb5e0faed2da0e232ef9b
Priyanshu0007 <63500076+Priyanshu0007@users.noreply.github.com>
2023-12-13 15:22:30 +0530

    message bubble added

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -6,10 +6,19 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var message:[Message]=[
+        Message(sender: "1@1.com", body: "Hey!"),
+        Message(sender: "2@1.com", body: "Helo!"),
+        Message(sender: "3@1.com", body: "Hola!"),
+        Message(sender: "4@1.com", body: "Hello!")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource=self
         title=K.appName
         navigationItem.hidesBackButton=true
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -25,3 +34,16 @@ class ChatViewController: UIViewController {
     }
     
 }
+extension ChatViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return message.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text=message[indexPath.row].body
+        return cell
+    }
+    
+}
+
